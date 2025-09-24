9a00286f93ea1b8987dee515d479a704e37396ea
Marko Karapandžić <karapandzic.it9.2021@uns.ac.rs>
2025-09-24 13:59:09 +0200

    ProductList, ProductCard, NavBar, Ligh/Dark mode...sec3

--- API/Program.cs
+++ API/Program.cs
@@ -8,14 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));  
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000","https://localhost:3001");
+});
 app.MapControllers();
 DbInitializer.InitDb(app);
 
