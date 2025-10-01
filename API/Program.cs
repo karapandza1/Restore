6bc523ae50b48811569c4a943a014d957fc4311b
Marko Karapandžić <karapandzic.it9.2021@uns.ac.rs>
2025-10-01 12:52:57 +0200

    Login, Register, Authorization, Authentification

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using API.Data;
+using API.Entities;
 using API.Middleware;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +18,13 @@ builder.Services.AddCors();
 
 builder.Services.AddTransient<ExceptionMiddleware>();
 
+builder.Services.AddIdentityApiEndpoints<User>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<StoreContext>();
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
@@ -23,7 +32,14 @@ app.UseCors(opt =>
 {
     opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:3000");
 });
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<User>(); //api/login
+
 DbInitializer.InitDb(app);
 
 app.Run();
